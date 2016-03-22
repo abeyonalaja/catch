5b7184144ab997a0ac8de471f913f8cf57b1986c
Abey Onalaja <abey.onalaja@gmail.com>
2016-03-22 23:51:09 +0000

    Added a firstr component skeleton

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+
+/*
+
+  StorePicker
+  this will let us make
+
+*/
+
+var StorePicker = React.createClass({
+
+    render: function() {
+        return (
+            <p>hello</p>
+        )
+    }
+});
